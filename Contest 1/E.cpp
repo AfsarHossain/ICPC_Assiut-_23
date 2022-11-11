f0e7d321d7e7ea7bd6e6ae6f2022033f638e6fad
AfsarHossain <afsar291722@gmail.com>
2022-11-12 00:50:03 +0600

    Contest 1 E no Code Added

---
+++ Contest 1/E.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll n, x, y;
+    cin>>n>>x>>y;
+
+    ll a, b, c;
+
+    a = n * x;
+    b = n * y;
+
+    cout<<b-a<<endl;
+}
